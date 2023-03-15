0fb1558138349fd92ef3957d03f826f885469671
shreydan <iamshreydan@gmail.com>
2023-03-15 20:23:45 +0530

    zero-shot inference

--- test.py
+++
@@ -1,9 +0,0 @@
-from liteclip import ZeroShotPipeline
-
-
-pipeline = ZeroShotPipeline()
-
-predictions = pipeline.predict('pup.jpg',['dog','person'])
-
-for label,prob in predictions:
-    print(f"{label}: {prob:.2f}")
