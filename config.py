86eeb3bb76329e630546a5182b57153a7f65d5a2
shreydan <iamshreydan@gmail.com>
2023-11-06 22:11:19 +0530

    convnext-tiny

--- config.py
+++ config.py
@@ -2,12 +2,12 @@ from dataclasses import dataclass
 
 @dataclass
 class Config:
-    text_encoder = 'model'
-    image_encoder = 'resnet50d'
+    text_encoder = './model'
+    image_encoder = 'convnext_tiny'
     
     # CLIP CONFIG
     proj_dim = 256
     dropout = 0.1
     max_length = 128
 
-    state_dict_path = 'model/clip_model.pt'
+    state_dict_path = './model/liteclip2.pt'
